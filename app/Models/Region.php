5e4192f76d0ff53bfc8dc6bbe75f09d50f996ead
borisvodka <borisblanchot@gmail.com>
2019-10-24 10:45:42 +0200

    1

---
+++ app/Models/Region.php
@@ -0,0 +1,13 @@
+<?php
+    namespace App\Models;
+    use Illuminate\Database\Eloquent\Model;
+    class Region extends Model
+    {
+        /**
+         * Get the ads for the region.
+         */
+        public function ads()
+        {
+            return $this->hasMany(Ad::class);
+        }
+    }
